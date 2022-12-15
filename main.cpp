d54ff8662a63d49f7f621ac59171c0557412a178
michael-s-b <michelsilva098@gmail.com>
2022-12-15 12:08:06 -0300

    initial commit with project pseudo-prototype

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// main function of the program
+int main(){
+    // the program starts
+    // show the user an menu with login and register options
+    // prompt the user to choose beetwen the options available
+    // if the user input is valid then call the corresponding function to the choice made
+    // login and register functions end with success or failure message
+};
